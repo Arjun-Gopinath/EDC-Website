e64d8f6a0637df0cf3ec8e00f1b09e389328086c
Rk1229 <38298009+Rk1229@users.noreply.github.com>
2018-08-14 19:37:55 +0530

    php created

--- iEDC Website/collect.php
+++ iEDC Website/collect.php
@@ -2,4 +2,74 @@
 
 <?php 
 echo $_POST["firstname"];
+echo $_POST["lastname"];
+echo $_POST["semester"];
+echo $_POST["branch"];
+echo $_POST["Email"];
+echo $_POST["mobile"];
+
+
+echo $_POST["cf1name"];
+echo $_POST["cf1lname"];
+echo $_POST["cf1sem"];
+echo $_POST["cf1branch"];
+echo $_POST["Email1"];
+echo $_POST["mobile1"];
+echo $_POST["NSSIAN"];
+
+
+echo $_POST["cf2name"];
+echo $_POST["cf2lname"];
+echo $_POST["cf2sem"];
+echo $_POST["cf2branch"];
+echo $_POST["Email2"];
+echo $_POST["mobile2"];
+echo $_POST["NSSIAN"];
+
+
+echo $_POST["cf3name"];
+echo $_POST["cf3lname"];
+echo $_POST["cf3sem"];
+echo $_POST["cf3branch"];
+echo $_POST["Email3"];
+echo $_POST["mobile3"];
+echo $_POST["NSSIAN"];
+
+
+echo $_POST["cf4name"];
+echo $_POST["cf4lname"];
+echo $_POST["cf4sem"];
+echo $_POST["cf4branch"];
+echo $_POST["Email4"];
+echo $_POST["mobile4"];
+echo $_POST["NSSIAN"];
+
+
+echo $_POST["cf5name"];
+echo $_POST["cf5lname"];
+echo $_POST["cf5sem"];
+echo $_POST["cf5branch"];
+echo $_POST["Email5"];
+echo $_POST["mobile5"];
+echo $_POST["NSSIAN"];
+
+
+echo $_POST["company"];
+echo $_POST["website"];
+echo $_POST["employee"];
+echo $_POST["desc"];
+echo $_POST["whopay"];
+echo $_POST["custpain"];
+echo $_POST["Competitors"];
+echo $_POST["UVP"];
+echo $_POST["solution"];
+echo $_POST["copyright"];
+echo $_POST["customers"];
+echo $_POST["marketing"];
+echo $_POST["makemoney"];
+echo $_POST["investment"];
+echo $_POST["Goals"];
+echo $_POST["docfile"];
+echo $_POST["imgfile"];
+
  ?>
